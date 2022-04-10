7b5595270457348082fcb8684f5336114ae56f07
Gabriela Sato <gabisato.ssb@gmail.com>
2022-04-10 15:35:56 -0300

    Adding react router and structuring it in the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SearchPage from "./pages/SearchPage/SearchPage";
+
+const App = () => {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
